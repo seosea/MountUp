fa0601348045aa4aeb6e616321219b3544d9260e
seosea <ssyaoao@naver.com>
2019-09-11 02:08:24 +0900

    Add : setting fragment view, revise user information activity view Revise : back button press, main color

--- app/src/main/java/com/example/mountup/Fragment/SettingFragment.java
+++ app/src/main/java/com/example/mountup/Fragment/SettingFragment.java
@@ -1,22 +1,57 @@
 package com.example.mountup.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.mountup.Activity.ReviseUserInformationActivity;
 import com.example.mountup.R;
 
-public class SettingFragment extends Fragment {
+public class SettingFragment extends Fragment implements View.OnClickListener{
+
+    Button btnUserInformation, btnLogout, btnLanguage;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_setting, container, false);
 
+        initView(view);
+        initListener();
+
         return view;
+    }
+
+    private void initView(View view){
+        btnLanguage = view.findViewById(R.id.btn_language_setting);
+        btnUserInformation = view.findViewById(R.id.btn_user_information_setting);
+        btnLogout = view.findViewById(R.id.btn_logout_setting);
+    }
+
+    private void initListener(){
+        btnLanguage.setOnClickListener(this);
+        btnUserInformation.setOnClickListener(this);
+        btnLogout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_language_setting:
+                break;
+            case R.id.btn_user_information_setting:
+                Intent intent = new Intent(this.getActivity(), ReviseUserInformationActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_logout_setting:
+                // TODO: 로그아웃 시
+                break;
+        }
     }
 }
