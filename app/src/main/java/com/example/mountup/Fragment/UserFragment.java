f5038d2909c1863b6d20b636fce3225eb9fb7669
seosea <ssyaoao@naver.com>
2019-09-20 03:29:04 +0900

    Add : user view, ID/pass Length

--- app/src/main/java/com/example/mountup/Fragment/UserFragment.java
+++ app/src/main/java/com/example/mountup/Fragment/UserFragment.java
@@ -17,7 +17,6 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import com.example.mountup.Adapter.ReviewAdapter;
 import com.example.mountup.Model.Review;
 import com.example.mountup.R;
-import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONArray;
 
@@ -27,25 +26,6 @@ public class UserFragment extends Fragment{
     @Override
     public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, final Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user, container, false);
-        ReviewAdapter adapter = new ReviewAdapter();
-
-        RecyclerView recyclerView = view.findViewById(R.id.rc_review_recyclerview);
-
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
-
-       
-
-        adapter.addItem(new Review("shin","이 산 너무 좋아요 다음에 또 오고싶어요. 이 산 너무 좋아요 다음에 또 오고싶어요. 이 산 너무 좋아요 다음에 또 오고싶어요. ",
-                BitmapFactory.decodeResource(getResources(),R.drawable.mt2),1004,(float)4.75,true));
-        adapter.addItem(new Review("shin","good",BitmapFactory.decodeResource(getResources(),R.drawable.mt),1004,4.35,true));
-        adapter.addItem(new Review("shin","good",BitmapFactory.decodeResource(getResources(),R.drawable.image),1004,3.1,false));
-        adapter.addItem(new Review("shin","good",BitmapFactory.decodeResource(getResources(),R.drawable.image),1004,1.0,false));
-        adapter.addItem(new Review("shin","good",BitmapFactory.decodeResource(getResources(),R.drawable.image),1004,0.1,false));
-        adapter.addItem(new Review("shin","good",BitmapFactory.decodeResource(getResources(),R.drawable.image),1004,0.0,false));
-        adapter.addItem(new Review("shin","good",BitmapFactory.decodeResource(getResources(),R.drawable.image),1004,2.0,false));
-
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapter);
 
         return view;
     }
